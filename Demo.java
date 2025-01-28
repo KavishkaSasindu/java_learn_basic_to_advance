7b54ebc9060817732573f74eedc53f8a18a3d2fe
Mudithananda KKS <131575180+KavishkaSasindu@users.noreply.github.com>
2025-01-28 03:43:57 +0000

    initilaize commit 1

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        String name = "Kavishka Sasindu";
+
+        System.out.println(name);
+    }
+}
