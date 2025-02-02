1ad5aa2ab72f77c92593e52f951528b0e4541872
Mudithananda KKS <131575180+KavishkaSasindu@users.noreply.github.com>
2025-02-02 04:59:08 +0000

    update abstract class

--- Demo.java
+++ Demo.java
@@ -1,6 +1,10 @@
 public class Demo {
     public static void main(String args[]) {
+
+        // you can not create object from abstract class
+        
         Define define = new Define();
         define.sayHello();
+        define.sayMyName("Kavishka Sasindu");
     }
 }
--- MyClass.java
+++ MyClass.java
@@ -1,4 +1,10 @@
+
+// to have an abstract method you should want abstract class
 public abstract class MyClass {
     public abstract void sayHello();
+
+    public void sayMyName(String name) {
+        System.out.println("My name is "+name);
+    }
     
 }
