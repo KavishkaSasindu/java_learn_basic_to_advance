53a80e466d66b1fa182a95c82814e591084731d8
Mudithananda KKS <131575180+KavishkaSasindu@users.noreply.github.com>
2025-02-02 05:01:01 +0000

    update main bracnh

--- Demo.java
+++ Demo.java
@@ -1,12 +1,5 @@
 public class Demo {
     public static void main(String args[]) {
-        System.out.println("Hello");
-        String name = "Kavishka Sasindu";
-
-        System.out.println(name);
-
-        Hello obj = new Hello();
-
-        System.out.println(obj.name);
+        
     }
 }
