8d1c2c4c50c1d69067029f8cbbc4e77d10b34f94
Mudithananda KKS <131575180+KavishkaSasindu@users.noreply.github.com>
2025-02-02 13:48:57 +0000

    learn exception basic

--- Demo.java
+++ Demo.java
@@ -1,5 +1,25 @@
 public class Demo {
     public static void main(String args[]) {
         
+        int i =0;
+        int j = 18;
+
+        int result ;
+        int sum = 0;
+
+        try{
+            result = j/i;
+            System.out.println(result);
+        }catch(ArithmeticException e){
+            System.out.println(e);
+            System.out.println("Something went Wrong");
+        }catch(Exception e){
+            System.out.println(e);
+        }finally{
+            sum = i+j;
+            System.out.println(sum);
+        }
+
+
     }
 }
